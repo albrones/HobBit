88546290380a3b384ab442b18f5ad388b660c642
Alexis B <alexis-contractor@playplay.com>
2022-10-08 12:16:50 +0200

    feat/dashboard: init with date of the day

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="dashboard">
+    <h2>Journal de bord: {{ dateOfDay }}</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+export default {
+  name: "DashboardCmp",
+  props: {},
+  setup() {
+    const dateOfDay = ref<string>("");
+
+    return { dateOfDay };
+  },
+  mounted() {
+    this.dateOfDay = new Date()
+      .toISOString()
+      .replace("-", "/")
+      .split("T")[0]
+      .replace("-", "/");
+  },
+};
+</script>
+
+<style>
+.dashboard {
+  display: flex;
+}
+</style>
